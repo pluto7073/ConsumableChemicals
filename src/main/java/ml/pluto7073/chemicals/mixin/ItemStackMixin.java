7e75f78805d1263b5ee35f075f15961a340d7487
pluto7073 <plutonia.electra.sagittaria@proton.me>
2026-04-05 20:22:52 -0600

    ItemStack injection fix

--- src/main/java/ml/pluto7073/chemicals/mixin/ItemStackMixin.java
+++ src/main/java/ml/pluto7073/chemicals/mixin/ItemStackMixin.java
@@ -44,7 +44,7 @@ public abstract class ItemStackMixin {
 							"Lnet/minecraft/world/level/Level;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V"),
 			method = "getTooltipLines"
 	)
-	private void chemicals$AddChemicalTooltipForFood(Player player, TooltipFlag context, CallbackInfoReturnable<List<Component>> cir, @Local(name = "list") List<Component> list) {
+	private void chemicals$AddChemicalTooltipForFood(Player player, TooltipFlag context, CallbackInfoReturnable<List<Component>> cir, @Local(ordinal = 0) List<Component> list) {
 		if (!getItem().isEdible() || getItem().getFoodProperties() == null) return;
 		if (!(context.isCreative() || context.isAdvanced())) return;
 		FoodProperties food = getItem().getFoodProperties();
@@ -61,7 +61,7 @@ public abstract class ItemStackMixin {
 							"Lnet/minecraft/world/level/Level;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V"),
 			method = "getTooltipLines"
 	)
-	private void chemicals$AddChemicalTooltipForChemicalContaining(Player player, TooltipFlag context, CallbackInfoReturnable<List<Component>> cir, @Local(name = "list") List<Component> list) {
+	private void chemicals$AddChemicalTooltipForChemicalContaining(Player player, TooltipFlag context, CallbackInfoReturnable<List<Component>> cir, @Local(ordinal = 0) List<Component> list) {
 		if (!(getItem() instanceof ChemicalContaining item)) return;
 		if (!(context.isCreative() || context.isAdvanced())) return;
 		if (player == null) return;
